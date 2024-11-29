50c2ba43637bac0dd03bd574495eb5ce3a75db34
Aksan07 <aksanpramod1@gmail.com>
2024-11-29 20:53:52 +0530

    monthly challenges done

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
+    path('',views.index),
     path('<int:month>',views.monthly_challenges_int),
-    path('<str:month>', views.monthly_challenges),
+    path('<str:month>', views.monthly_challenges,name="month-challenge"),
     
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges_dic = {
     "january":"Learn",
@@ -12,18 +13,34 @@ monthly_challenges_dic = {
     "august":"Live for God",
     "september": "Stay Humble", 
     "october":"He didnot abandon you",
-    "november":"Sacrifice the evil in your hurt",
+    "november":"Sacrifice the evil in your heart",
     "december":"Kill Lust",
                       }
 
 # Create your views here.
+def index(request):
+    list_items =''
+    month_key =list(monthly_challenges_dic.keys())
+    for month in month_key:
+        month_url =reverse("month-challenge",args=[month])
+        list_items+=f'<li><a href="{month_url}">{month}</a></li>'
+    return HttpResponse(f"<ul>{list_items}</ul>")
+
 
 def monthly_challenges_int(request,month):
-    return HttpResponse(month)
+    
+    months = list(monthly_challenges_dic.keys())
+    if month >len(months):
+        return HttpResponseNotFound("Invalid Month")
+    redirect_month = months[month-1]
+    redirect_url =reverse("month-challenge",args=[redirect_month])
+    
+    return HttpResponseRedirect(redirect_url)
 
 def monthly_challenges(request,month):
     try:
         challenge_text = monthly_challenges_dic[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except KeyError:
-        return HttpResponseNotFound("Believe Man, God Bless Your Soul")
+        return HttpResponseNotFound("<h1>Believe Man, God Bless Your Soul</h1>")
