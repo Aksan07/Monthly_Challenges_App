24eec631d6b38ee94f289531346f5c6f4ab98a44
Aksan07 <aksanpramod1@gmail.com>
2024-11-29 11:28:39 +0530

    monthly challenges 10%

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('january', views.index)
+    path('<int:month>',views.monthly_challenges_int),
+    path('<str:month>', views.monthly_challenges),
+    
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
+
+monthly_challenges_dic = {
+    "january":"Learn",
+    "february":"Work Hard",
+    "march": "Pray Hard",
+    "april": "Keep Going",
+    "may":"Fight On",
+    "june":"Understand Defeat",
+    "july":"Become Better",
+    "august":"Live for God",
+    "september": "Stay Humble", 
+    "october":"He didnot abandon you",
+    "november":"Sacrifice the evil in your hurt",
+    "december":"Kill Lust",
+                      }
 
 # Create your views here.
-def index (request):
-    return HttpResponse("Hi This Works.")
+
+def monthly_challenges_int(request,month):
+    return HttpResponse(month)
+
+def monthly_challenges(request,month):
+    try:
+        challenge_text = monthly_challenges_dic[month]
+        return HttpResponse(challenge_text)
+    except KeyError:
+        return HttpResponseNotFound("Believe Man, God Bless Your Soul")
